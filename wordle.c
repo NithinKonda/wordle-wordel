61273002894e8ba63c6552723a4163705835ed83
NithinKonda <nithinkonda142@gmail.com>
2024-11-17 23:05:10 +0530

    feat: Add Result and cc

---
+++ wordle.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#define ResultGreen 1
+#define ResultYellow 2
+#define ResultRed 5
+
+// struct s_word {
+//     char word[8];
+// }
+
+
+struct s_result {
+
+}
+
+
+Result checkchar(char,char,int,char*);
+
+int main();
+
+Result checkchar(char correct, char guess, int idx, char *word)
+{
+
+}
+
+
+int main()
+{
+    printf("abc\n");
+    return 0;
+}
