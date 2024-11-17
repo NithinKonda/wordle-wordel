3dfe1f1b1a211eecc9d790f0821e2c6388dd262a
NithinKonda <nithinkonda142@gmail.com>
2024-11-17 23:14:51 +0530

    feat: Add Result CC

--- wordle.c
+++ wordle.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #define ResultGreen 1
 #define ResultYellow 2
 #define ResultRed 5
@@ -10,21 +10,32 @@
 //     char word[8];
 // }
 
-
-struct s_result {
-
+struct s_result
+{
+    char color[5];
 }
 
+typedef char Result;
+typedef Result[5] Results;
 
-Result checkchar(char,char,int,char*);
+Result checkchar(char, int, char *);
 
 int main();
 
-Result checkchar(char correct, char guess, int idx, char *word)
+Result checkchar(char guess,  int idx, char *word)
 {
-
+    char correct;
+    correct = word[idx];
+    switch (word[idx]) {
+        case correct:
+            return ResultGreen;
+        default:
+            if (isin(correct, word))
+                return ResultYellow;
+    }
+    return ResultRed;
+    
 }
-
 
 int main()
 {
