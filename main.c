a866d2e7557b6c700c1c39aa6cffeeb0783ff34d
NithinKonda <nithinkonda142@gmail.com>
2024-11-23 00:35:05 +0530

    feat: Add ret words

--- main.c
+++ main.c
@@ -12,13 +12,12 @@
 // }
 typedef char Result;
 
-
-struct s_words {
+struct s_words
+{
     char **arr;
     int ren;
 
 }
-
 
 typedef struct s_result Words;
 struct s_result
@@ -26,135 +25,149 @@ struct s_result
     char color[5];
 };
 
-
-
-bool isin(char, char*);
+bool isin(char, char *);
 void Example_printing_the_results(Result[]);
 Result *checkword(char *, char *);
 Result checkchar(char, int, char *);
 
-int main(int, char**);
+int main(int, char **);
 
-
-void Example_printing_the_results(Result res[]) {
+void Example_printing_the_results(Result res[])
+{
     int i;
-    for (i=0; i<5; i++) 
-        switch(res[i]) {
-            case ResultGreen:
-                printf("%s\n","Green");
-                break;
-            case ResultYellow:
-                printf("%s\n","Yellow");
-                break;
-            case ResultRed:
-                printf("%s\n","Red");
-                break;
-            default:
-                printf("Unknown %d\n",res[i]);
-                break;
-
-    }
+    for (i = 0; i < 5; i++)
+        switch (res[i])
+        {
+        case ResultGreen:
+            printf("%s\n", "Green");
+            break;
+        case ResultYellow:
+            printf("%s\n", "Yellow");
+            break;
+        case ResultRed:
+            printf("%s\n", "Red");
+            break;
+        default:
+            printf("Unknown %d\n", res[i]);
+            break;
+        }
 }
 
-
-bool isin(char c, char *str){
-    int i,size;
+bool isin(char c, char *str)
+{
+    int i, size;
     bool ret;
     ret = false;
     size = strlen(str);
 
-    for(i=0;i<size;i=i+1)
+    for (i = 0; i < size; i = i + 1)
     {
-        if (str[i] == c){
+        if (str[i] == c)
+        {
             ret = true;
             break;
         }
-
-
     }
     return ret;
 }
-Result checkchar(char guess,  int idx, char *word)
+Result checkchar(char guess, int idx, char *word)
 {
     char correct;
     correct = word[idx];
-    if (guess == correct){
+    if (guess == correct)
+    {
         return ResultGreen;
     }
-        else if (isin(guess, word))
-                return ResultYellow;
+    else if (isin(guess, word))
+        return ResultYellow;
     return ResultRed;
-    
 }
 
-Words readfile(char *filename, int max) {
+Words readfile(char *filename, int max)
+{
 
-char buf[8];
-int i,size;
-FILE *fd;
-char *ret[5];
-Words words;
-fd = fopen(filename, "r");
-if (!fd) {
-    perror("fopen");
-    return (char **)0;
-}
-
-
-size = max*5;
-ret = (char **)malloc(size);
-if(!ret) {
-    fclose(fd);
-    perror("malloc");
-    
-
-    return (char **)0;
-
-    i=0;
-
-    memset(buf,0,8);
-    while (fgets(buf, 7, fd)) {
-        size = strlen(buf);
-        if (size < 1) {
-            memset(buf,0,8);
-            continue;
-        }
-
-
-
-        size--;
-        buf[size]=0;
-
-        if (size != 0) {
-            memset(buf, 0,8);
-            continue;
-        }
-        ret[i][0]=buf[0];
-        ret[i][1]=buf[1];
-        ret[i][2]=buf[2];
-        ret[i][3]=buf[3];
-        ret[i][4]=buf[4];
-
-        memset(buf,0,8);
-        n++;
-
-
-
-        if(max <= n){
-            break;
-        }
+    char buf[8];
+    int i, size;
+    FILE *fd;
+    char *ret[5];
+    Words words;
+    fd = fopen(filename, "r");
+    if (!fd)
+    {
+        perror("fopen");
+        words = {
+            .arr = (char **)0,
+            .n=0
+        };
+        return words;
     }
 
-    fclose(fd);
-}
+    size = max * 5;
+    ret = (char **)malloc(size);
+    if (!ret)
+    {
+        fclose(fd);
+        perror("malloc");
+
+        return (char **)0;
+        words = {
+            .arr = (char **)0,
+            .n=0
+        };
+        i = 0;
+        return words;
+    }
+        i=0;
+        memset(buf, 0, 8);
+        while (fgets(buf, 7, fd))
+        {
+            size = strlen(buf);
+            if (size < 1)
+            {
+                memset(buf, 0, 8);
+                continue;
+            }
+
+            size--;
+            buf[size] = 0;
+
+            if (size != 0)
+            {
+                memset(buf, 0, 8);
+                continue;
+            }
+            ret[i][0] = buf[0];
+            ret[i][1] = buf[1];
+            ret[i][2] = buf[2];
+            ret[i][3] = buf[3];
+            ret[i][4] = buf[4];
+
+            memset(buf, 0, 8);
+            n++;
+
+            if (max <= n)
+            {
+                break;
+            }
+        }
+
+        fclose(fd);
+        words = {
+            .arr = ret,
+            .n =i
+        };
+
+        return words;
 
 }
-Result *checkword(char *guess, char *word){
+Result *checkword(char *guess, char *word)
+{
     static Result res[5];
     int i;
 
-    for (i=0; i<5;i++) {
-        res[i]= checkchar(guess[i],i,word);
+    for (i = 0; i < 5; i++)
+    {
+        res[i] = checkchar(guess[i], i, word);
     }
     return res;
 }
@@ -163,13 +176,12 @@ int main(int argc, char *argv[])
 {
     char *correct, *guess;
     Result *res;
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr,
-        "Usage: %s THE WORD IS CORRECT/WRONG \n",argv[0]
-        );
+                "Usage: %s THE WORD IS CORRECT/WRONG \n", argv[0]);
         return -1;
     }
-
 
     correct = argv[1];
     guess = argv[2];
